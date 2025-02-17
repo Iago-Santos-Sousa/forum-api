1945cbf2c6f36e4ffd9980ee5d8de99d5a1fc32b
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2025-02-16 23:31:06 -0300

    feat: add DTOs and entities for questions and answers, and implement services and modules

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,25 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
 import { Prisma, User as UserModel } from "@prisma/client";
 import { UserService } from "./user.service";
+import { AuthGuard } from "src/auth/auth.guard";
 
 @Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post("signup")
+  @Post()
   async signupUser(@Body() userData: Prisma.UserCreateInput): Promise<UserModel> {
     return this.userService.createUser(userData);
   }
 
+  @UseGuards(AuthGuard)
   @Get(":id")
   async getUser(@Param("id") id: string): Promise<UserModel> {
     return this.userService.user({ id: Number(id) });
   }
 
-  @Put(":id")
+  @UseGuards(AuthGuard)
+  @Patch(":id")
   async updateUser(
     @Body() userData: Prisma.UserUpdateInput,
     @Param("id") id: string,
@@ -24,6 +27,7 @@ export class UserController {
     return this.userService.updateUser({ where: { id: Number(id) }, data: userData });
   }
 
+  @UseGuards(AuthGuard)
   @Delete(":id")
   async deleteUser(@Param("id") id: string): Promise<UserModel> {
     return this.userService.deleteUser({ id: Number(id) });
